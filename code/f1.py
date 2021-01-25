da42336462a3261655b072ec586b674872fc15b6
Eric Boris <ericboris@tutanota.com>
2021-01-25 13:56:47 -0800

    implement spec bare minimum predictor

--- code/f1.py
+++ code/f1.py
@@ -1,12 +1,3 @@
-# Model
-# Get and return top 3 letters.
-# Manage a timer.
-# Manage quit/send.
-# Need to determine standard data transfer format. Are we using dicts? or np.array? or something else?
-# Handle load data - train, valid, test
-# Train a model
-# Make a prediction
-# Update from prediction and make next prediction
 class Model:
 	''' Provide a consistent interface for getting letters from a Predictor. '''
 	def next_letters():
@@ -31,7 +22,6 @@ class Model:
 			Trigger generating performance and timing results. '''
 		pass
 
-# Sends and receives data from the model.
 class Predictor:
 	''' Provide an abstract parent class for predictors to implement
 		so that we can swap in different predictors for different results. '''
@@ -67,9 +57,6 @@ class MLE(Predictor):
 		pass
 """
 
-# View
-# Handle how the predicted letters are presented.
-# Probably as minimal as print(f'{char1}\t{char2}\t{char3}')
 class View:
 	''' Provide a consistent interface for displaying results from the Model. '''
 	def show():
@@ -77,8 +64,6 @@ class View:
 			as well as "Quit?" and "Send?" options. '''
 		pass
 
-# Controller
-# Accept inputs and return the results to the model. 
 class Controller
 	''' Provide a consistent interface for sending input to the Model. '''
 	def get_input():
