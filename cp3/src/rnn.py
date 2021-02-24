7c333c5ee6e1949305b884456a7ec1e5322e6693
Eric Boris <ericboris@tutanota.com>
2021-02-23 19:08:58 -0800

    try new model

--- cp3/src/rnn.py
+++ cp3/src/rnn.py
@@ -22,18 +22,16 @@ def get_vocabulary(data):
 			vocabulary += char
 	return vocabulary
 
-def random_chunk(data, chunk_len):
-    start_index = random.randint(0, file_len - chunk_len)
-    end_index = start_index + chunk_len + 1
-    return data[start_index:end_index]
-
 class RNN(nn.Module):
-	def __init__(self, input_size, hidden_size, output_size, n_layers=1):
+	def __init__(self, input_size, hidden_size, output_size, vocabulary, learning_rate, n_layers=1, chunk_len=200):
 		super(RNN, self).__init__()
 		self.input_size = input_size
 		self.hidden_size = hidden_size
 		self.output_size = output_size
 		self.n_layers = n_layers
+		self.chunk_len = chunk_len
+		self.vocabulary = vocabulary
+		self.learning_rate = learning_rate
 
 		self.encoder = nn.Embedding(input_size, hidden_size)
 		self.gru = nn.GRU(hidden_size, hidden_size, n_layers)
@@ -48,30 +46,53 @@ class RNN(nn.Module):
 	def init_hidden(self):
 		return Variable(torch.zeros(self.n_layers, 1, self.hidden_size))
 
-	def train(self, data):
+	def random_chunk(self, data):
+		start_index = random.randint(0, len(data) - self.chunk_len)
+		end_index = start_index + self.chunk_len + 1
+		return data[start_index:end_index]
+
+	def random_training_set(self, chunk):
+		inp = self.char_tensor(chunk[:-1])
+		target = self.char_tensor(chunk[1:])
+		return inp, target
+
+	def char_tensor(self, string):
+		# Turn string into list of longs
+		tensor = torch.zeros(len(string)).long()
+		for c in range(len(string)):
+			if string[c] in self.vocabulary:
+				tensor[c] = self.vocabulary.index(string[c])
+			else:
+				tensor[c] = self.input_size
+		return Variable(tensor)
+
+	def train(self, data, n_epochs):
 		self.data = data	
 		start = time.time()
 		all_losses = []
 		loss_avg = 0
+
+		decoder_optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+		criterion = nn.CrossEntropyLoss()
 
 		def inner(inp, target):
 			hidden = self.init_hidden()
 			self.zero_grad()
 			loss = 0
 
-			for c in range(chunk_len):
+			for c in range(self.chunk_len):
 				output, hidden = self(inp[c], hidden)
 				target = torch.reshape(target, (len(target), 1))
 				loss += criterion(output, target[c])
 
 			loss.backward()
 			decoder_optimizer.step()
-			return loss.data.item() / chunk_len
+			return loss.data.item() / self.chunk_len
 
 		for epoch in range(1, n_epochs + 1):
-			chunk = random_chunk(data, chunk_len)
+			chunk = self.random_chunk(data)
 
-			inp, target = random_training_set(chunk)
+			inp, target = self.random_training_set(chunk)
 			loss = inner(inp, target)     
 			loss_avg += loss
 
@@ -83,48 +104,33 @@ class RNN(nn.Module):
 				all_losses.append(loss_avg / plot_every)
 				loss_avg = 0
 
-# Turn string into list of longs
-def char_tensor(string):
-    tensor = torch.zeros(len(string)).long()
-    for c in range(len(string)):
-        if string[c] in all_characters:
-            tensor[c] = all_characters.index(string[c])
-        else:
-            tensor[c] = UNK_INDEX
-    return Variable(tensor)
+	# Returns the top 3 next characters for a given string
+	def evaluate(self, prime_str='A', temperature=0.8):
+		hidden = decoder.init_hidden()
+		prime_input = char_tensor(prime_str)
 
-def random_training_set(chunk):
-	inp = char_tensor(chunk[:-1])
-	target = char_tensor(chunk[1:])
-	return inp, target
+		# Use priming string to "build up" hidden state
+		for p in range(len(prime_str) - 1):
+			_, hidden = decoder(prime_input[p], hidden)
+		inp = prime_input[-1]
 
-# Returns the top 3 next characters for a given string
-def evaluate(prime_str='A', temperature=0.8):
-    hidden = decoder.init_hidden()
-    prime_input = char_tensor(prime_str)
+		output, hidden = decoder(inp, hidden)
 
-    # Use priming string to "build up" hidden state
-    for p in range(len(prime_str) - 1):
-        _, hidden = decoder(prime_input[p], hidden)
-    inp = prime_input[-1]
+		# Sample from the network as a multinomial distribution
+		output_dist = output.data.view(-1).div(temperature).exp()
+		top_4 = torch.multinomial(output_dist, 4)
 
-    output, hidden = decoder(inp, hidden)
+		# this should go through and skip the UNK character if it is a top 3 character
+		top_3 = []
+		for i in range(4):
+			if top_4[i] != self.input_size:
+				top_3.append(self.vocabulary[top_4[i]])
 
-    # Sample from the network as a multinomial distribution
-    output_dist = output.data.view(-1).div(temperature).exp()
-    top_4 = torch.multinomial(output_dist, 4)
-
-    # this should go through and skip the UNK character if it is a top 3 character
-    top_3 = []
-    for i in range(4):
-        if top_4[i] != UNK_INDEX:
-            top_3.append(all_characters[top_4[i]])
-
-    predicted_char_1 = top_3[0]
-    predicted_char_2 = top_3[1]
-    predicted_char_3 = top_3[2]
-    predicted = predicted_char_1 + predicted_char_2 + predicted_char_3
-    return predicted
+		predicted_char_1 = top_3[0]
+		predicted_char_2 = top_3[1]
+		predicted_char_3 = top_3[2]
+		predicted = predicted_char_1 + predicted_char_2 + predicted_char_3
+		return predicted
 
 def time_since(since):
     s = time.time() - since
@@ -157,14 +163,7 @@ def load_test_data(fname):
 			data.append(inp)
 	return data
 
-if __name__ == '__main__':
-	parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-	parser.add_argument('mode', choices=('train', 'test'), help='what to run')
-	parser.add_argument('--work_dir', help='where to save', default='work')
-	parser.add_argument('--test_data', help='path to test data', default='example/input.txt')
-	parser.add_argument('--test_output', help='path to write test predictions', default='pred.txt')
-	args = parser.parse_args()
-
+def main(args):
 	if args.mode == 'train':	
 		if not os.path.isdir(args.work_dir):
 			print(f'Making working directory {args.work_dir}')
@@ -172,7 +171,6 @@ if __name__ == '__main__':
 		
 		if os.path.isfile(path=join(args.work_dir, 'model.checkpoint')):
 			print('Trained model already exists')
-			# decoder = read('../work/trained_model')
 		else:
 			print('Instantiating model')
 
@@ -182,25 +180,23 @@ if __name__ == '__main__':
 			plot_every = 10
 			hidden_size = 100
 			n_layers = 2
-			lr = 0.005
+			learning_rate = 0.005
 			chunk_len = 100
 
 			print('Loading training data')
 			
 			data = open('data/cleaned_data/train.txt').read()
-			file_len = len(data)
+			#file_len = len(data)
 	
-			all_characters = get_vocabulary(data)
+			vocabulary = get_vocabulary(data)
+			n_characters = len(vocabulary)
 
-			n_characters = len(all_characters) + 1
-			UNK_INDEX = len(all_characters)
+			#UNK_INDEX = len(vocabulary)
 
 			print('Training')
-			decoder = RNN(n_characters, hidden_size, n_characters, n_layers)
+			decoder = RNN(n_characters, hidden_size, n_characters, vocabulary, learning_rate, n_layers, chunk_len)
 
-			decoder_optimizer = torch.optim.Adam(decoder.parameters(), lr=lr)
-			criterion = nn.CrossEntropyLoss()
-			decoder.train(data)
+			decoder.train(data, n_epochs)
 
 			print('Saving model')
 			write(file_name=join(args.work_dir, 'model.checkpoint'), obj=decoder)
@@ -210,8 +206,8 @@ if __name__ == '__main__':
 		
 		print(f'Loading test data from {args.test_data}')
 		test_data = load_test_data(args.test_data)
-		all_characters = get_vocabulary(decoder.data)
-		UNK_INDEX = len(all_characters)
+		vocabulary = get_vocabulary(decoder.data)
+		#UNK_INDEX = len(vocabulary)
 
 		print('Making predictions')
 		results = []
@@ -224,3 +220,15 @@ if __name__ == '__main__':
 		write_pred(results, args.test_output)
 	else:
 		raise NotImplementedError(f'Unknown mode {args.mode}')
+
+if __name__ == '__main__':
+	# Prepare the arguments.
+	parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+	parser.add_argument('mode', choices=('train', 'test'), help='what to run')
+	parser.add_argument('--work_dir', help='where to save', default='work')
+	parser.add_argument('--test_data', help='path to test data', default='example/input.txt')
+	parser.add_argument('--test_output', help='path to write test predictions', default='pred.txt')
+
+	# Run the program.
+	main(parser.parse_args())
+
